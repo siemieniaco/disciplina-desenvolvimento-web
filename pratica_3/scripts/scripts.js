25e56a3f5e085bc9d3f5b30d5258c1790f2ebf38
Daniel <dsiemieniaco@hotmail.com>
2021-03-01 14:52:59 -0300

    pratica 3

---
+++ pratica_3/scripts/scripts.js
@@ -0,0 +1,33 @@
+let minhaImagem = document.querySelector('img');
+
+minhaImagem.onclick = function() {
+    let meuSrc = minhaImagem.getAttribute('src');
+    if(meuSrc === 'images/Firefox.png') {
+      minhaImagem.setAttribute ('src','images/Firefox_2.png');
+    } else {
+      minhaImagem.setAttribute ('src','images/Firefox.png');
+    }
+}
+
+let meuBotao = document.querySelector('button');
+let meuCabecalho = document.querySelector('h1');
+
+function defineNomeUsuario() {
+    let meuNome = prompt('Por favor, digite seu nome.');
+    if(!meuNome || meuNome === null) {
+      defineNomeUsuario();
+    } else {
+      localStorage.setItem('nome', meuNome);
+      meuCabecalho.innerHTML = 'Mozilla é legal, ' + meuNome;
+    }
+  }
+
+  if(!localStorage.getItem('nome')) {
+    defineNomeUsuario();
+  } else {
+    let nomeGuardado = localStorage.getItem('nome');
+    meuCabecalho.textContent = 'Mozilla é legal, ' + nomeGuardado;
+  }
+
+  meuBotao.onclick = function() { defineNomeUsuario();
+  }
